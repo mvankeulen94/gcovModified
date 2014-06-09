689e894bb16088549e44cd4466df4f8777e23f87
Maria van Keulen <mvankeulen94@gmail.com>
2014-06-08 20:43:18 -0400

    Renamed list.c, created list.h

--- list.c
+++ list.c
@@ -1,85 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "list.h"
 
-/* This is a collection of functions designed to store JSON data
- * in a linked list.
- */
-struct List {
-    // list pointers
-    struct List *next;
-    struct List *end;
-    struct List *prev;
-    struct List *begin;
-
-    // data pointer
-    struct ListData *data;
+void initList (struct List *list) {
+    list->front = NULL;
+    list->back = NULL;
 }
 
-struct ListData {
-    char *key;
-    void *value;
-    char *type;
+int isEmptyList (struct List *list) {
+    return list->front == NULL;
 }
 
-void initList(struct List *list) {
-    list->next = NULL;
-    list->end = NULL;
-    list->begin = NULL;
-    list->prev = NULL;
-    list->data = NULL;
-}
-
-int isEmptyList(struct List *list) {
-    return list->end == NULL;
-}
-
-void addToList(struct List *list, char *key, void *value, char *type) {
-    struct List *newList = malloc(sizeof(struct List));
-    newList->data = malloc(sizeof(struct ListData));
-    newList->data->key = key;
-    newList->data->value = value;
-    newList->data->type = type;
-
+void addToList (struct List *list, char *key, void *value, char *type) {
+    struct Node *newNode = malloc(sizeof(struct Node));
+   
     if (isEmptyList(list)) {
-        list->end = newList;
-        list->begin = newList;
+        list->front = newNode; 
+        list->back = newNode;
     }
 
     else {
-        list->end->next = newList;
-        list->end = newList;
-    }
-} 
-
-void deleteList(struct List *list) {
-    struct List *next = current->next;
-    struct List *current = list->begin;
-    if (strcmp(current->data->type, "list") == 0) {
-        deleteList((struct List *) current->data->value);
+        list->back->next = newNode;
+        list->back = newNode;
     }
 
-    free(current->data);
-    free(current);
-
-    if (current == list->end) {
-        return;
+    newNode->key = key;
+    newNode->value = value;
+    newNode->type = type;
+    newNode->next = NULL;
+}
+static void deleteNode (struct Node *node) {
+    int proceed = 1;
+    struct Node *next = node->next;
+    printf("%s\n", node->type);
+    
+    if (next == NULL) {
+        proceed = 0;
     }
 
-    else {
-        deleteList(next);
+    if (strcmp(node->type, "list") == 0) {
+        deleteList((struct List *) node->value);
+        printf("finished sublist delete\n");
+    }
+
+        printf("about to delete %s\n", node->type);
+        free(node);
+        printf("deleted from list\n");
+
+    if (proceed) {
+        deleteNode(next);
     }
 }
+        
+void deleteList (struct List *list) {
+    deleteNode (list->front);
+}
+
 
 int main() {
-    struct List list;
+     struct List list;
     initList(&list);
-    addToList(&list, "h", 1, "int");
+    addToList(&list, "h", "1", "int");
     struct List sublist;
     initList(&sublist);
     addToList(&sublist, "hello", "lala", "string");
     addToList(&list, "yo", &sublist, "list");
     addToList(&list, "well", "huh?", "string");
+    printf("%s\n", (char *) list.front->value);
+    struct Node *next = (list.front)->next;
+    printf("%s\n", next->type);
+    printf("%s\n", (char *) next->next->value);
+    printf("%s\n", (char *) (list).back->value);
+
     deleteList(&list);
-}
+  return 0;
+} 
+
 
---
+++ list.h
@@ -0,0 +1,18 @@
+struct Node {
+    struct Node *next;
+
+    char *key;
+    void *value;
+    char *type;
+};
+
+struct List {
+    struct Node *front;
+    struct Node *back;
+};
+
+void deleteList(struct List *);
+void initList(struct List *list);
+int isEmptyList(struct List *list);
+void addToList(struct List *list, char *key, void *value, char *type);
+void deleteList(struct List *list);
