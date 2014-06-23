9716f6dfdd67c20f0aa6e188f5c121faf0c59e21
Maria van Keulen <mvankeulen94@gmail.com>
2014-06-23 14:59:07 +0000

    Removed unnecessary lines

--- jsonTransmission/jsonConnect.py
+++ jsonTransmission/jsonConnect.py
@@ -79,47 +79,7 @@ def doJSONImport():
     http_client.close()
 
 def doJSONAggregate(body):
-    parser = optparse.OptionParser(usage="""\
-                                   %prog [database] [collection] [filename]
-                                   [gitHash] [buildHash]""")
-
-    # add in command line options. Add mongo host/port combo later
-    parser.add_option("-f", "--filename", dest="fname",
-                      help="name of file to import",
-                      default=None)
-    parser.add_option("-g", "--githash", dest="ghash",
-                      help="git hash of code being tested",
-                      default=None)
-    parser.add_option("-b", "--buildhash", dest="bhash", 
-                      help="build hash of code being tested",
-                      default=None)
-
-    parser.add_option("-c", "--connectionstring", dest="connectstr",
-                      help="string specifying url",
-                      default=None)
-
-    (options, args) = parser.parse_args()
-    
-    if options.fname is None:
-        print "\nERROR: Must specify name of file to import\n"
-        sys.exit(-1)
-   
-    if options.ghash is None:
-        print "\nERROR: Must specify git hash \n"
-        sys.exit(-1)
-    
-    if options.bhash is None:
-        print "\nERROR: Must specify build hash \n"
-        sys.exit(-1)
- 
-    if options.connectstr is None:
-        print "\nERROR: Must specify connection string \n"
-        sys.exit(-1)
-   
     http_client = tornado.httpclient.HTTPClient()
-    record = {"gitHash": options.ghash, "buildHash": options.bhash}
-
-       
     if body == "full":
         request = tornado.httpclient.HTTPRequest(
                              url="http://127.0.0.1:8888/report",
